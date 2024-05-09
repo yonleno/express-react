d944162b7e2f05d225ed52d5bfae5764b43bff97
Hector Parra <phector040@gmail.com>
2024-05-08 21:22:57 -0400

    Defined a server route to return a message when the client makes a get request

--- server/index.js
+++ server/index.js
@@ -1,3 +1,9 @@
 const express = require('express')
 const app = express()
 const port = 3000
+
+// define our server routes
+app.get('/test', (req, res)=>{
+    //Return the message "Server is operational" if a client makes a get request to the route /test
+    res.send('Server is operational');
+})
