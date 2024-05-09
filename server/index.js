103620c27a935fef79f10c57c7ab57f386a0f2d9
Hector Parra <phector040@gmail.com>
2024-05-08 21:23:49 -0400

    Run our sserver to listen at the defined port

--- server/index.js
+++ server/index.js
@@ -7,3 +7,8 @@ app.get('/test', (req, res)=>{
     //Return the message "Server is operational" if a client makes a get request to the route /test
     res.send('Server is operational');
 })
+
+app.listen(port, ()=>{
+    console.log(`Server running on port ${port}`)
+})
+
