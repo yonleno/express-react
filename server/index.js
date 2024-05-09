5a35d59a06e18bbcaf2a914d466c2c2ccec6e07a
Hector Parra <phector040@gmail.com>
2024-05-08 21:25:33 -0400

    Allowed static assets from public

--- server/index.js
+++ server/index.js
@@ -2,6 +2,8 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+app.use(express.static('public'))
+
 // define our server routes
 app.get('/test', (req, res)=>{
     //Return the message "Server is operational" if a client makes a get request to the route /test
